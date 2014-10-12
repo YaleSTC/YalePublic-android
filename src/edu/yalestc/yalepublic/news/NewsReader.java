357f84e9a7b752166df24fbb9447c52769a0324e
Jason Liu <jason@jasonkliu.com>
2014-10-12 13:05:47 -0400

    Programmatically add all of the fields to ArrayLists to be displayed

--- src/edu/yalestc/yalepublic/news/NewsReader.java
+++ src/edu/yalestc/yalepublic/news/NewsReader.java
@@ -8,7 +8,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.xml.sax.SAXException;
@@ -17,6 +19,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 import edu.yalestc.yalepublic.R;
 import edu.yalestc.yalepublic.news.RssReader;
@@ -29,6 +32,7 @@ import edu.yalestc.yalepublic.news.RssFeed;
 public class NewsReader extends Activity {
 
     TextView tRSSTitle, tRSSContent;
+    RssFeed feed;
 
     // Check for connectivity, return true if connected or connecting.
     public boolean isOnline() {
@@ -46,12 +50,51 @@ public class NewsReader extends Activity {
         tRSSTitle = (TextView) findViewById(R.id.tvRSSTitle);
         tRSSContent = (TextView) findViewById(R.id.tvRSSContent);
         
-        // TODO: Check for Internet Connectivity first
+        // If we're online, downloads the RSS Feed and returns it as `feed`
         if (isOnline()) {
             NewsDownload start = new NewsDownload();
-            start.execute("http://news.yale.edu/news-rss");
+            try {
+                feed = start.execute("http://news.yale.edu/news-rss").get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         } else {
             Log.d("NewsReader", "Please connect to Internet");
+        }
+
+        if (feed != null) {       // EHOSTUNREACH: No route to hoast
+            ArrayList<RssItem> rssItems = feed.getRssItems();
+            ArrayList<String> rssTitles = new ArrayList<String>();
+            ArrayList<String> rssLinks = new ArrayList<String>();
+            ArrayList<String> rssDescription = new ArrayList<String>();
+            ArrayList<String> rssContent = new ArrayList<String>();
+
+            /*private String title;
+            private String link;
+            private Date pubDate;
+            private String description;
+            private String content;*/
+
+            for (RssItem rssItem : rssItems) {
+                Log.d("RSS Reader", rssItem.getTitle());
+                rssTitles.add(rssItem.getTitle());
+                rssLinks.add(rssItem.getLink());
+                rssDescription.add(rssItem.getDescription());
+                rssContent.add(rssItem.getContent());
+            }
+
+            /*String[] video_arrays = {"video1", "video2"};
+            List<String> videos = new ArrayList<String>(Arrays.asList(video_arrays)); */
+
+            // Parameters: Activity (Context), Layout file, Id of TextView, Array that's adapted
+            final ArrayAdapter<String> mNewsAdapter;
+            mNewsAdapter = new ArrayAdapter<String>(
+                    this, R.layout.news_tab, R.id.tvTitle, rssTitles);
+
+            ListView listView = (ListView) findViewById(R.id.listNews);
+            listView.setAdapter(mNewsAdapter);
         }
     }
 
