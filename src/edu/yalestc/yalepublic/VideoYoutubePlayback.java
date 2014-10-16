7635d5ebccfab5f7d074318c2e7231488b12036b
Ebstein <stanislaw.swid@gmail.com>
2014-10-16 10:32:53 -0400

    First commit of new class

---
+++ src/edu/yalestc/yalepublic/VideoYoutubePlayback.java
@@ -0,0 +1,14 @@
+package edu.yalestc.yalepublic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideoYoutubePlayback extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_list);
+    }
+
+}
