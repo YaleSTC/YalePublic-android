de300cd709e06855cb695d270cd63b91df926670
Jason Liu <jason@jasonkliu.com>
2014-10-30 11:36:47 -0400

    Replace tabs with spaces in RssItem

--- src/edu/yalestc/yalepublic/news/RssItem.java
+++ src/edu/yalestc/yalepublic/news/RssItem.java
@@ -27,23 +27,23 @@ import android.os.Parcelable;
 
 public class RssItem implements Comparable<RssItem>, Parcelable {
 
-	private RssFeed feed;
-	private String title;
-	private String link;
-	private String dateString;
-	private Date pubDate;
-	private String description;
-	private String content;
+    private RssFeed feed;
+    private String title;
+    private String link;
+    private String dateString;
+    private Date pubDate;
+    private String description;
+    private String content;
 
-	public RssItem() {
-		
-	}
-	
-	public RssItem(Parcel source) {
-		
-		Bundle data = source.readBundle();
-		title = data.getString("title");
-		link = data.getString("link");
+    public RssItem() {
+        
+    }
+    
+    public RssItem(Parcel source) {
+        
+        Bundle data = source.readBundle();
+        title = data.getString("title");
+        link = data.getString("link");
         dateString = data.getString("dateString");
         try {
             // Date format: "Fri, 17 Oct 2014 11:47:00 EDT"
@@ -53,61 +53,61 @@ public class RssItem implements Comparable<RssItem>, Parcelable {
             e.printStackTrace();
         }
         description = data.getString("description");
-		content = data.getString("content");
-		feed = data.getParcelable("feed");
-		
-	}
+        content = data.getString("content");
+        feed = data.getParcelable("feed");
+        
+    }
 
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		
-		Bundle data = new Bundle();
-		data.putString("title", title);
-		data.putString("link", link);
-		data.putSerializable("pubDate", pubDate);
-		data.putString("description", description);
-		data.putString("content", content);
-		data.putParcelable("feed", feed);
-		dest.writeBundle(data);
-	}
-	
-	public static final Parcelable.Creator<RssItem> CREATOR = new Parcelable.Creator<RssItem>() {
-		public RssItem createFromParcel(Parcel data) {
-			return new RssItem(data);
-		}
-		public RssItem[] newArray(int size) {
-			return new RssItem[size];
-		}
-	};
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        
+        Bundle data = new Bundle();
+        data.putString("title", title);
+        data.putString("link", link);
+        data.putSerializable("pubDate", pubDate);
+        data.putString("description", description);
+        data.putString("content", content);
+        data.putParcelable("feed", feed);
+        dest.writeBundle(data);
+    }
+    
+    public static final Parcelable.Creator<RssItem> CREATOR = new Parcelable.Creator<RssItem>() {
+        public RssItem createFromParcel(Parcel data) {
+            return new RssItem(data);
+        }
+        public RssItem[] newArray(int size) {
+            return new RssItem[size];
+        }
+    };
 
-	@Override
-	public int describeContents() {
-		return 0;
-	}
-	
-	public RssFeed getFeed() {
-		return feed;
-	}
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+    
+    public RssFeed getFeed() {
+        return feed;
+    }
 
-	public void setFeed(RssFeed feed) {
-		this.feed = feed;
-	}
+    public void setFeed(RssFeed feed) {
+        this.feed = feed;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public String getLink() {
-		return link;
-	}
+    public String getLink() {
+        return link;
+    }
 
-	public void setLink(String link) {
-		this.link = link;
-	}
+    public void setLink(String link) {
+        this.link = link;
+    }
 
     public String getDateString() {
         return dateString;
@@ -117,46 +117,46 @@ public class RssItem implements Comparable<RssItem>, Parcelable {
         this.dateString = newdate;
     }
 
-	public Date getPubDate() {
-		return pubDate;
-	}
+    public Date getPubDate() {
+        return pubDate;
+    }
 
-	public void setPubDate(Date pubDate) {
-		this.pubDate = pubDate;
-	}
+    public void setPubDate(Date pubDate) {
+        this.pubDate = pubDate;
+    }
 
-	public void setPubDate(String pubDate) {
-		try {
-			SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
-			this.pubDate = dateFormat.parse(pubDate);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-	}
+    public void setPubDate(String pubDate) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH);
+            this.pubDate = dateFormat.parse(pubDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getContent() {
-		return content;
-	}
+    public String getContent() {
+        return content;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-	@Override
-	public int compareTo(RssItem another) {
-		if(getPubDate() != null && another.getPubDate() != null) {
-			return getPubDate().compareTo(another.getPubDate());
-		} else { 
-			return 0;
-		}
-	}
-	
+    @Override
+    public int compareTo(RssItem another) {
+        if(getPubDate() != null && another.getPubDate() != null) {
+            return getPubDate().compareTo(another.getPubDate());
+        } else { 
+            return 0;
+        }
+    }
+    
 }
