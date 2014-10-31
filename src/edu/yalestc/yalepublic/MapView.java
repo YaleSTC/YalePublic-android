8b5c0fdc19ce14b28660bd2ac7c4563b693610ee
Jason Liu <jason@jasonkliu.com>
2014-10-30 20:44:57 -0400

    Add MapView simple layout

---
+++ src/edu/yalestc/yalepublic/MapView.java
@@ -0,0 +1,17 @@
+package edu.yalestc.yalepublic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jason Liu on 10/18/14.
+ */
+public class MapView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_simple);
+    }
+
+}
