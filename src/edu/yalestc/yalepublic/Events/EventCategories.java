2a7ba789424f3e91a4aa8eaa23537a79985923ec
Ebstein <stanislaw.swidwinski@yale.edu>
2014-11-04 22:58:09 -0500

    Added a custom adapter but not yet done

--- src/edu/yalestc/yalepublic/Events/EventCategories.java
+++ src/edu/yalestc/yalepublic/Events/EventCategories.java
@@ -3,6 +3,7 @@ package edu.yalestc.yalepublic.Events;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,11 +12,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import edu.yalestc.yalepublic.R;
 
 public class EventCategories extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +48,12 @@ public class EventCategories extends Activity {
 
     private class EventsCategoriesAdapter extends BaseAdapter {
         String[] categories = getResources().getStringArray(R.array.events_category_names);
+        Context mContext;
+
+        EventsCategoriesAdapter(Context context){
+            mContext = context;
+        }
+
         @Override
         public int getCount() {
             return categories.length;
@@ -62,10 +71,17 @@ public class EventCategories extends Activity {
 
         @Override
         public View getView(int i, View convertView, ViewGroup viewGroup) {
-            if (convertView == null){
-            convertView.
+            if(convertView != null){
+                ((ImageView)((RelativeLayout) convertView).getChildAt(0)).setImageBitmap(); //ADD THE TRUE COLOR / RECTANGLE
+                ((TextView)((RelativeLayout) convertView).getChildAt(1)).setText(categories[i]);
+                return ((RelativeLayout)convertView);
             } else {
-
+                //if not, create a new one from the template of a view using inflate
+                LayoutInflater inflater = (LayoutInflater)mContext.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+                RelativeLayout button = ((RelativeLayout)inflater.inflate(R.layout.thumbnail_elements,null));
+                ((ImageView)button.getChildAt(0)).setImageBitmap();//ADD THE TRUE COLOR / RECTANGLE
+                ((TextView)button.getChildAt(1)).setText(categories[i]);
+                return button;
             }
         }
     }
