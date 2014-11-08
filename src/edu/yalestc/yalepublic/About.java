49b12285c26da9b855c450b27810ac6463c7c5f9
Jason Liu <jason@jasonkliu.com>
2014-11-08 11:35:26 -0500

    Add about screen and to Manifest

---
+++ src/edu/yalestc/yalepublic/About.java
@@ -0,0 +1,17 @@
+package edu.yalestc.yalepublic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Jason Liu on 11/8/14.
+ */
+public class About extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_screen);
+    }
+}
+
