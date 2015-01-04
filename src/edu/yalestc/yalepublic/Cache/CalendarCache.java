cd30a54a1ed5a21c24bacefe07aaefb6f2ac10ce
Ebstein <stanislaw.swidwinski@yale.edu>
2015-01-04 08:43:43 -0500

    Extends JSONReader to avoid executing asyncTasks within AsyncTasks

--- src/edu/yalestc/yalepublic/Cache/CalendarCache.java
+++ src/edu/yalestc/yalepublic/Cache/CalendarCache.java
@@ -1,5 +1,7 @@
 package edu.yalestc.yalepublic.Cache;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -24,24 +26,60 @@ import edu.yalestc.yalepublic.Videos.JSONReader;
 /**
  * Created by Stan Swidwinski on 12/15/14.
  */
-public class CalendarCache{
+
+/*
+    Few words on how this class works. First of all it extends JSONReader, so it has access to all its
+protected and public classes (see the constructor -> you need to "super" the inherited class!). Hence,
+We can pull data from anywhere using this thread and we do not have to use additional threads (note that
+in Android you can only execute an AsyncTask (spawn a thread) from the UI Thread (here the MainActivity),
+so this is almost the only possibility we have.
+
+    This class can be read like a class that does not extend anything but for the part where I use super.setURL()
+and super.getData(). Note That the JSONReader class has been changed to allow for it (the functions are abstracted
+away from doInBackground())!
+ */
+public class CalendarCache extends JSONReader {
     Context mContext;
     Calendar myCalendar;
     SharedPreferences prefs;
-    private EventsParseForDateWithinCategory dataParser;
-    private String[] responses;
+    int year;
+    int month;
+    private ProgressDialog dialog;
 
     public CalendarCache(Context context){
-            //for creating and getting preferences and tables!
+        super(context);
+        //for creating and getting preferences and tables!
         mContext = context;
+        dialog = new ProgressDialog(mContext);
+        dialog.setCancelable(false);
+        dialog.setMessage("Obsolete information detected. Please wait while the newest information is downloaded. This may take a while...");
+        dialog.setTitle("Updating...");
+        dialog.setIndeterminate(true);
         myCalendar = Calendar.getInstance();
-        int month = myCalendar.get(Calendar.MONTH);
-        int year = myCalendar.get(Calendar.YEAR);
+        month = myCalendar.get(Calendar.MONTH);
+        year = myCalendar.get(Calendar.YEAR);
         prefs = mContext.getSharedPreferences("events",0);
+    }
+
+    @Override
+    protected void onPreExecute(){
+        dialog.show();
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
         if(prefs.contains("dateCached")){
             //check if the cached data is up-to-date and update if it is not.
         } else {
-           createDatabaseForTheFirstTime(year, month);
+            createDatabaseForTheFirstTime(year, month);
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        if(dialog != null && dialog.isShowing()){
+            dialog.dismiss();
         }
     }
 
@@ -71,13 +109,14 @@ public class CalendarCache{
     }
 
         //month and year needed to query for events on given day. can be refactored by changing the EventsParseForDateWithinCategory class.
-    private void parseAndSaveToDb (String rawData, int month, int year){
+    private void parseAndSaveToDb (String rawData, int year, int month){
         CalendarDatabaseTableHandler eventTable = new CalendarDatabaseTableHandler(mContext);
             //the 0 at the end means that we disregard the categories and just get all of them.
         EventsParseForDateWithinCategory parser = new EventsParseForDateWithinCategory(rawData, month, year, mContext, 0);
             //brute force all days
         for(int i = 1; i < 32; i++){
             String date = Integer.toString(year) + dateFormater.formatMonthFromCalendarFormat(month) + dateFormater.formatDayFromCalendarFormat(i);
+            Log.i("Cache", "date " + date);
             ArrayList<String[]> validEvents = parser.getEventsOnGivenDate(date);
             if(validEvents.size() > 0){
                 for(String[] event : validEvents){
@@ -93,35 +132,26 @@ public class CalendarCache{
         //for later parsing purposes. EventsParseForDateWithinCategory has to discard nonsense events
         //and for that needs year and month (YYYYMM)
         int[] yearMonth;
-        JSONReader[] dataPullers = new JSONReader[4];
         //create the preferences
         updatePreferences(year, month);
-        //prepare the queries and JSONReaders for -1 month up to +2 months
+        //prepare the queries and JSONReaders for -1 month up to +2 months and get info
         queries = new String[4];
         yearMonth = new int[4];
+        results = new String[4];
         for (int i = 0; i < 4; i++){
             yearMonth[i] = dateFormater.toYearMonth(year, month + i -1);
             queries[i] = "http://calendar.yale.edu/feeds/feed/opa/json/" + dateFormater.formatDateForJSONQuery(year, month + i - 1) + "/30days";
-            dataPullers[i] = new JSONReader(queries[i], mContext);
-            Log.i("CalendarCache","Created puller with query" + queries[i]);
-        }
-        //try to execute and collect the results
-        results = new String[4];
-
-        try {
-            for(int i = 0; i < 4; i++){
-                results[i] = dataPullers[i].execute().get();
-                if (results[i] == null) {
-                    Toast toast = new Toast(mContext);
-                    toast = Toast.makeText(mContext, "You need internet connection to succesfully finish", Toast.LENGTH_LONG);
-                    toast.show();
-                    return;
-                }
+            super.setURL(queries[i]);
+            results[i] = super.getData();
+            Log.i("Cache", "contents of result[" + Integer.toString(i) + "]:" + results[i].substring(0, 100));
+            // Log.i("CacheUpdater","Created puller with query" + queries[i]);
+            // Log.i("CacheUpdater", "yearMonth " + yearMonth[i]);
+            if (results[i] == null) {
+                Toast toast = new Toast(mContext);
+                toast = Toast.makeText(mContext, "You need internet connection to succesfully finish", Toast.LENGTH_LONG);
+                toast.show();
+                return;
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
         }
 
         for(int i = 0; i < 4; i++)
