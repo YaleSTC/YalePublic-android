6cdbe9c0b1342de42288d7df0b3c1e0145daa983
Ebstein <stanislaw.swidwinski@yale.edu>
2015-01-10 18:45:01 -0500

    Dialog shows when data is pulled about playlists

--- src/edu/yalestc/yalepublic/Videos/VideoList.java
+++ src/edu/yalestc/yalepublic/Videos/VideoList.java
@@ -29,13 +29,7 @@ import edu.yalestc.yalepublic.R;
 //**
 
 public class VideoList extends Activity {
-    // this is a class parameter so that it can be modified in the asynctask
-    private PlaylistAdapter adapter;
-    //this is a string in which we store the ID's of playlists to pass them
-    //into VideosWithinPlaylist
-    private String[] playlistIds;
-    private String[] allPlaylists;
-    private String rawData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,96 +51,24 @@ public class VideoList extends Activity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_video_list,
                     container, false);
 
             // create an asynctask that fetches the playlist titles. It should speak for itself.
             //Just note that in constructor we give in the base url (WITHOUT "?" at the end).
-            JSONReader scrapeData = new JSONReader("https://www.googleapis.com/youtube/v3/playlists", getActivity());
-            scrapeData.addParams(new Pair<String, String>("part","snippet"));
-            scrapeData.addParams(new Pair<String, String>("channelId","UC4EY_qnSeAP1xGsh61eOoJA"));
-            scrapeData.addParams(new Pair<String, String>("key",new DeveloperKey().DEVELOPER_KEY));
-            scrapeData.addParams(new Pair<String, String>("maxResults","50"));
+            VideosJSONReader scrapeData = new VideosJSONReader("https://www.googleapis.com/youtube/v3/playlists", getActivity());
+            scrapeData.addParams(new Pair<String, String>("part", "snippet"));
+            scrapeData.addParams(new Pair<String, String>("channelId", "UC4EY_qnSeAP1xGsh61eOoJA"));
+            scrapeData.addParams(new Pair<String, String>("key", new DeveloperKey().DEVELOPER_KEY));
+            scrapeData.addParams(new Pair<String, String>("maxResults", "50"));
 
-            try {
-                 rawData = scrapeData.execute().get();
-                //rawData is null if there are problems. We get a toast for no internet!
-                 if(rawData == null){
-                    Toast toast = new Toast(getActivity());
-                    toast = Toast.makeText(getActivity(), "You need internet connection to view the content!", Toast.LENGTH_LONG);
-                    toast.show();
-                    finish();
-                     return null;
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_video);
+            scrapeData.addListView(listView);
+            scrapeData.execute();
 
-            if(getPlaylistsFromJson(rawData)) {
-                // initialize the ArrayAdapter
-                adapter  = new PlaylistAdapter(getActivity(), allPlaylists);
-                ListView listView = (ListView) rootView.findViewById(R.id.listview_video);
-                listView.setAdapter(adapter);
-                //set OnItemClickListener to open up a new activity in which we get
-                //all the videos listed
-                listView.setOnItemClickListener(new OnItemClickListener() {
+            return rootView;
 
-                    @Override
-                    public void onItemClick(AdapterView<?> arg0, View arg1,
-                                            int arg2, long arg3) {
-                        //redirect to new activity displaying all videos
-                        Intent showThem = new Intent(VideoList.this, VideosWithinPlaylist.class);
-                        showThem.putExtra("playlistId", playlistIds[arg2]);
-                        //For Debug purposes - show what is the playlistID
-                        Log.d("StartingActivityInVideoList", playlistIds[arg2]);
-                        startActivity(showThem);
-                    }
-                });
-                return rootView;
-            } else {
-                return null;
-            }
         }
     }
-
-    private boolean getPlaylistsFromJson(String rawData){
-        JSONObject videoData;
-        try {
-            videoData = new JSONObject(rawData);
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return false;
-        }
-        JSONArray playlistData;
-        try {
-            playlistData = videoData.getJSONArray("items");
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return false;
-        }
-
-        allPlaylists = new String[playlistData.length()];
-        //we need to remember playlistIDs for future processing!
-        playlistIds = new String[playlistData.length()];
-        for (int i = 0; i < playlistData.length(); i++){
-            try {
-                allPlaylists[i] = playlistData.getJSONObject(i)
-                        .getJSONObject("snippet")
-                        .getString("title");
-                playlistIds[i] = playlistData.getJSONObject(i)
-                        .getString("id");
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return false;
-            }
-        }
-        return true;
-    }
-    
 }
---
+++ src/edu/yalestc/yalepublic/Videos/VideosJSONReader.java
@@ -0,0 +1,152 @@
+package edu.yalestc.yalepublic.Videos;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.yalestc.yalepublic.JSONReader;
+
+/**
+ * Created by Stan Swidwinski on 1/10/15.
+ */
+
+public class VideosJSONReader extends JSONReader {
+
+    Activity mActivity;
+    private ProgressDialog dialog;
+    private String mRawData;
+    private PlaylistAdapter mAdapter;
+    private ListView mListView;
+    private String[] playlistIds;
+    String[] allPlaylists;
+
+
+    public VideosJSONReader(Activity activity){
+        super(activity);
+        //for creating and getting preferences and tables!
+        mActivity = activity;
+        dialog = new ProgressDialog(mActivity);
+        dialog.setCancelable(false);
+        dialog.setTitle("Fetching the videos!");
+        dialog.setMessage("This should not take too long, please wait...");
+        dialog.setIndeterminate(true);
+        mAdapter = null;
+        mListView = null;
+    }
+
+    public VideosJSONReader(String URL, Activity activity){
+        super(URL, activity);
+        mActivity = activity;
+        dialog = new ProgressDialog(mActivity);
+        dialog.setCancelable(false);
+        dialog.setTitle("Fetching the videos!");
+        dialog.setMessage("This should not take too long, please wait...");
+        dialog.setIndeterminate(true);
+        mAdapter = null;
+        mListView = null;
+    }
+
+    @Override
+    protected void onPreExecute(){
+        dialog.show();
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        mRawData = super.getData();
+        if (mRawData == null) {
+            Toast toast = new Toast(mActivity);
+            toast = Toast.makeText(mActivity, "You need internet connection to view the content!", Toast.LENGTH_LONG);
+            toast.show();
+            Log.i("CalendarFragment", "Failure");
+            mActivity.finish();
+            return null;
+        } else {
+            Log.i("CalendarFragment", "Success");
+        }
+        return mRawData;
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        if(mListView != null){
+            getPlaylistsFromJson(mRawData);
+            mAdapter = new PlaylistAdapter(mActivity, allPlaylists);
+            display();
+        }
+        if(dialog != null && dialog.isShowing()){
+            dialog.dismiss();
+        }
+    }
+
+    public void addListView(ListView listView){
+        mListView = listView;
+    }
+
+    private void display(){
+        mListView.setAdapter(mAdapter);
+        //set OnItemClickListener to open up a new activity in which we get
+        //all the videos listed
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int arg2, long arg3) {
+                //redirect to new activity displaying all videos
+                Intent showThem = new Intent(mActivity, VideosWithinPlaylist.class);
+                showThem.putExtra("playlistId", playlistIds[arg2]);
+                //For Debug purposes - show what is the playlistID
+                Log.d("StartingActivityInVideoList", playlistIds[arg2]);
+                mActivity.startActivity(showThem);
+            }
+        });
+
+    }
+
+    private boolean getPlaylistsFromJson(String rawData){
+        JSONObject videoData;
+        try {
+            videoData = new JSONObject(rawData);
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+        JSONArray playlistData;
+        try {
+            playlistData = videoData.getJSONArray("items");
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+        allPlaylists = new String[playlistData.length()];
+        //we need to remember playlistIDs for future processing!
+        playlistIds = new String[playlistData.length()];
+        for (int i = 0; i < playlistData.length(); i++){
+            try {
+                allPlaylists[i] = playlistData.getJSONObject(i)
+                        .getJSONObject("snippet")
+                        .getString("title");
+                playlistIds[i] = playlistData.getJSONObject(i)
+                        .getString("id");
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+}
