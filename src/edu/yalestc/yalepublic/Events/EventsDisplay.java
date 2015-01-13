0ad0c056dc1afe0215407f9baf6eea29710959ba
Ebstein <stanislaw.swidwinski@yale.edu>
2015-01-13 13:54:14 -0500

    settings do not show

--- src/edu/yalestc/yalepublic/Events/EventsDisplay.java
+++ src/edu/yalestc/yalepublic/Events/EventsDisplay.java
@@ -77,25 +77,6 @@ public class EventsDisplay extends Activity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.events_display, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void pullNewDataFromInternet(){
         dataPuller = new JSONReader("http://calendar.yale.edu/feeds/feed/opa/json/" + currentDate +"-01"+ "/30days", this);
 
