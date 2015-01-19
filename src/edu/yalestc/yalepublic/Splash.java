d0bda28637fbe113873ac2d954c30df14a7c92d1
Carsten Peterson <carsten.peterson@yale.edu>
2015-01-19 14:37:43 -0500

    created a splash screen

---
+++ src/edu/yalestc/yalepublic/Splash.java
@@ -0,0 +1,36 @@
+package edu.yalestc.yalepublic;
+
+/**
+ * Created by carstenpeterson on 1/19/15.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+public class Splash extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Log.d("Splash", "Starting Main Activity");
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
