83da21c13c8b6b99cc4a682f91cc7c0526e3ba8b
Jason Liu <jason@jasonkliu.com>
2015-01-24 15:08:53 -0500

    Add MapSearch file

---
+++ src/edu/yalestc/yalepublic/MapSearch.java
@@ -0,0 +1,45 @@
+package edu.yalestc.yalepublic;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by Jason Liu on 10/18/14.
+ */
+public class MapSearch extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionbar = getActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);     // Show home as a back arrow
+        //actionbar.setDisplayShowHomeEnabled(true);     // Show application logo
+        actionbar.setDisplayShowTitleEnabled(true);    // Show activity title/subtitle
+        actionbar.setDisplayUseLogoEnabled(false);     // Use activity logo instead of activity icon
+        actionbar.setTitle(getString(R.string.maps));  // Set title
+
+        setContentView(R.layout.map_simple);
+
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng yale = new LatLng(41.3111, -72.9267);   // The - signifies the Western Hemisphere
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(yale, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Yale")
+                .snippet("The best university ever.")
+                .position(yale));
+    }
+
+}
