e8861428f3b37077db8b70e4069fdc06972556e6
Jason Liu <jason@jasonkliu.com>
2016-05-01 23:18:25 -0400

    Remove old VideoTask

--- src/edu/yale/yalepublic/PhotoList.java
+++ src/edu/yale/yalepublic/PhotoList.java
@@ -34,28 +34,14 @@ import java.util.List;
 
 public class PhotoList extends Activity {
 
-    public static final String PHOTO_ID_KEY = "playlistId";
-    public enum Mode {
-        VIDEO,
-        PHOTO,
-        EMPTY
-    }
-    // this is a class parameter so that it can be modified in the asynctask
-    private ArrayAdapter<String> mVideoAdapter;    //TODO: Refactor
-    //this is a string in which we store the ID's of playlists to pass them
-    //into VideosWithinPlaylist
-    private String[] playlistIds;                //TODO: Refactor
-    private Mode mode;
-    TextView loading;
     ProgressBar spinner;
-    VideoTask videoList;
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         Log.d("PhotoList", "backPressed");
-        if (videoList != null)
-            videoList.cancel(true);
+        //if (videoList != null)
+          //  videoList.cancel(true);
     }
 
     @Override
@@ -66,7 +52,6 @@ public class PhotoList extends Activity {
         spinner = (ProgressBar) findViewById(R.id.pbLoading);
         spinner.setVisibility(View.GONE);
 
-
         Bundle extras = getIntent().getExtras();
         PlaceholderFragment placeholderFragment = new PlaceholderFragment();
         placeholderFragment.setArguments(extras);
@@ -75,166 +60,4 @@ public class PhotoList extends Activity {
                 .replace(R.id.photoContainer, placeholderFragment).commit();
     }
 
-
-    // Parses the raw data (which is a String in JSON format) and extracts the titles of
-    // the photo albums to display in a ListView.
-    public class VideoTask extends AsyncTask<Void, Void, String[]> {
-
-        private String[] getPlaylistsFromJson(String rawData){
-           
-            JSONObject videoData;
-            JSONArray playlistData = null;
-            try {
-                videoData = new JSONObject(rawData);
-                switch(mode){
-                case VIDEO:
-                    playlistData = videoData.getJSONArray("items");
-                    break;
-                case PHOTO:
-                    playlistData = videoData.getJSONObject("photosets")
-                                    .getJSONArray("photoset");
-                    break;
-                default:
-                    break;
-                
-                }
-                
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                return null;
-            }
-            
-            String[] allPlaylists = new String[playlistData.length()];
-            //we need to remember playlistIDs for future processing!
-            int playlistDataLength = playlistData.length();
-            playlistIds = new String[playlistDataLength];
-            for (int i = 0; i < playlistDataLength; i++){
-                try {
-                    switch(mode){
-                    case VIDEO:
-                        allPlaylists[i] = playlistData.getJSONObject(i)
-                        .getJSONObject("snippet")
-                        .getString("title");
-                        break;
-                    case PHOTO:
-                        allPlaylists[i] = playlistData.getJSONObject(i)
-                        .getJSONObject("title")
-                        .getString("_content");
-                        break;
-                    default:
-                        break;
-                    }
-                    playlistIds[i] = playlistData.getJSONObject(i)
-                            .getString("id");
-                } catch (JSONException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            return allPlaylists;
-        }
-        
-        @Override
-        protected String[] doInBackground(Void... params) {
-            Uri builtUri = null;
-            try{
-                // first we create the URI
-                switch(mode){
-                case VIDEO:
-                    builtUri = getVideoAPIRequestUri();
-                    break;
-                case PHOTO:
-                    builtUri = getPhotoAPIRequestUri();
-                    break;
-                default:
-                    return null;  // #135, this should never happen. Extras not passed correctly
-                }
-                
-                // send a GET request to the server
-                if (builtUri == null)
-                    return null;
-                URL url = new URL(builtUri.toString());
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // read all the data
-                InputStream inputStream = urlConnection.getInputStream();                
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                String videosJsonStr = buffer.toString();
-                // we pass the data to getPlaylistsFromJson
-                //but also remember to save the playlistID's for future
-                return getPlaylistsFromJson(videosJsonStr);
-                
-                // TODO check if there are more than 50 videos in the arrays (not for photos)
-            }
-            
-            catch (IOException e){
-                Log.e("URI", "uri was invalid or api request failed");
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        // Returns a URI in the form (https://api.flickr.com/...) for use in polling the API
-        // to get the list of albums from the Yale Flickr account.
-        private Uri getPhotoAPIRequestUri() {
-            final String USER_ID = "12208415@N08";    //Yale flickr user id
-            final String BASE_URL = "https://api.flickr.com/services/rest/?";
-            //TODO: extract api key and secret
-            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
-                        .appendQueryParameter("method", "flickr.photosets.getList")
-                        .appendQueryParameter("api_key", new DeveloperKey().FLICKR_API_KEY)
-                        .appendQueryParameter("user_id", USER_ID) 
-                        .appendQueryParameter("format", "json")
-                        .appendQueryParameter("nojsoncallback", "1")
-                        .build();
-            return builtUri;
-        }
-
-        // Returns a URI in the form (https://googleapis.com/youtube/...) for use in polling the API
-        // to get the list of playlists from the Yale Youtube account.
-        private Uri getVideoAPIRequestUri() {
-            final String BASE_URL = "https://www.googleapis.com/youtube/v3/playlists?";
-            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
-                    .appendQueryParameter("part", "snippet")
-                    .appendQueryParameter("channelId", "UC4EY_qnSeAP1xGsh61eOoJA")
-                    .appendQueryParameter("key", new DeveloperKey().DEVELOPER_KEY)
-                    .appendQueryParameter("maxResults", "50")
-                    .build();
-            return builtUri;
-        }
-        
-        @Override
-        protected void onPostExecute(String[] result){
-            // we need to use result in our ArrayAdapter; adds all of the resulting values.
-            //spinner.setVisibility(View.GONE);
-            loading.setVisibility(View.GONE);  // Hide the progress
-            if (result != null) {
-                List<String> videos = new ArrayList<String>(Arrays.asList(result));
-                mVideoAdapter.addAll(videos);
-            } else {
-                Toast.makeText(getApplicationContext(), "You need internet connection to view the content!", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
 }
